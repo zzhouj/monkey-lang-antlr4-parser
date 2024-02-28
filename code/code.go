4a67239585a77e9d5a0df638cf8fda20277c3a17
Justin <457299@qq.com>
2024-02-28 12:54:55 +0800

    compiler support hash

--- code/code.go
+++ code/code.go
@@ -30,6 +30,7 @@ const (
 	OpSetGlobal
 	OpGetGlobal
 	OpArray
+	OpHash
 )
 
 type Definition struct {
@@ -57,6 +58,7 @@ var difinitions = map[OpCode]*Definition{
 	OpSetGlobal:     {"OpSetGlobal", []int{2}},
 	OpGetGlobal:     {"OpGetGlobal", []int{2}},
 	OpArray:         {"OpArray", []int{2}},
+	OpHash:          {"OpHash", []int{2}},
 }
 
 func Lookup(op byte) (*Definition, error) {
