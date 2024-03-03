d8300648ae08908868b138a26bdeb59a834d70c3
Justin <457299@qq.com>
2024-03-04 00:11:59 +0800

    more testing for closure

--- vm/vm_test.go
+++ vm/vm_test.go
@@ -527,9 +527,80 @@ func TestClosures(t *testing.T) {
 	runVmTests(t, []vmTestCase{
 		{
 			`
-			fn(a) {
-				fn() {a}
-			}(99)()`,
+			let newClosure = fn(a) {
+				fn() { a; };
+			};
+			let closure = newClosure(99);
+			closure();
+			`,
+			99,
+		},
+		{
+			`
+			let newAdder = fn(a, b) {
+				fn(c) { a + b + c };
+			};
+			let adder = newAdder(1, 2);
+			adder(8);
+			`,
+			11,
+		},
+		{
+			`
+			let newAdder = fn(a, b) {
+				let c = a + b;
+				fn(d) {
+					c + d;
+				};
+			};
+			let adder = newAdder(1, 2);
+			adder(8);
+			`,
+			11,
+		},
+		{
+			`
+			let newAdderOuter = fn(a, b) {
+				let c = a + b;
+				fn(d) {
+					let e = c + d;
+					fn(f) {
+						e + f;
+					};
+				};
+			};
+			let newAdder = newAdderOuter(1, 2);
+			let adder = newAdder(3);
+			adder(8);
+			`,
+			14,
+		},
+		{
+			`
+			let a = 1;
+			let newAdderOuter = fn(b) {
+				fn(c) {
+					fn(d) {
+						a + b + c + d;
+					};
+				};
+			};
+			let newAdder = newAdderOuter(2);
+			let adder = newAdder(3);
+			adder(8);
+			`,
+			14,
+		},
+		{
+			`
+			let newClosure = fn(a, b) {
+				let one = fn() { a }
+				let two = fn() { b }
+				fn() { one() + two() };
+			};
+			let closure = newClosure(9, 90);
+			closure();
+			`,
 			99,
 		},
 	})
