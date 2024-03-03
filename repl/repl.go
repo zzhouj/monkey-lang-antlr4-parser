ec386cc010047ac3ce23416fc6e09ffbf84052d5
Justin <457299@qq.com>
2024-03-03 15:22:04 +0800

    Update repl.go

--- repl/repl.go
+++ repl/repl.go
@@ -18,6 +18,9 @@ func Start(in io.Reader, out io.Writer) {
 
 	constants := []object.Object{}
 	symbolTable := compiler.NewSymbolTable()
+	for i, builtin := range object.Builtins {
+		symbolTable.DefineBuiltin(i, builtin.Name)
+	}
 	globals := make([]object.Object, vm.GlobalsSize)
 
 	for {
