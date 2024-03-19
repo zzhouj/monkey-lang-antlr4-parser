af3b9654a3f4f998096e95f798b10836649743c4
Justin <457299@qq.com>
2024-03-19 20:02:28 +0800

    saving intermediate result in node of parse tree

--- parser_antlr4/parser.go
+++ parser_antlr4/parser.go
@@ -15,7 +15,7 @@ type Parser struct {
 	parser *monkey.MonkeyParser
 
 	errors []string
-	stk    []any
+	values map[antlr.ParserRuleContext]any
 }
 
 func New(input string) *Parser {
@@ -23,8 +23,11 @@ func New(input string) *Parser {
 	parser := monkey.NewMonkeyParser(antlr.NewCommonTokenStream(lexer, antlr.TokenDefaultChannel))
 
 	p := &Parser{
-		BaseMonkeyListener: &monkey.BaseMonkeyListener{},
-		parser:             parser,
+		BaseMonkeyListener:   &monkey.BaseMonkeyListener{},
+		DefaultErrorListener: &antlr.DefaultErrorListener{},
+		parser:               parser,
+		errors:               []string{},
+		values:               map[antlr.ParserRuleContext]any{},
 	}
 
 	lexer.RemoveErrorListeners()
@@ -36,17 +39,12 @@ func New(input string) *Parser {
 	return p
 }
 
-func (p *Parser) push(n any) {
-	p.stk = append(p.stk, n)
+func (p *Parser) setValue(ctx antlr.ParserRuleContext, value any) {
+	p.values[ctx] = value
 }
 
-func (p *Parser) pop() any {
-	if len(p.stk) > 0 {
-		result := p.stk[len(p.stk)-1]
-		p.stk = p.stk[:len(p.stk)-1]
-		return result
-	}
-	return nil
+func (p *Parser) getValue(ctx antlr.ParserRuleContext) any {
+	return p.values[ctx]
 }
 
 func (p *Parser) ParseProgram() *ast.Program {
@@ -57,7 +55,7 @@ func (p *Parser) ParseProgram() *ast.Program {
 		}
 	}
 	antlr.NewParseTreeWalker().Walk(p, prog)
-	return p.pop().(*ast.Program)
+	return p.getValue(prog).(*ast.Program)
 }
 
 func (p *Parser) Errors() []string {
@@ -75,21 +73,21 @@ func (p *Parser) VisitErrorNode(node antlr.ErrorNode) {
 func (p *Parser) ExitProg(ctx *monkey.ProgContext) {
 	n := len(ctx.AllStat())
 	stats := make([]ast.Statement, n)
-	for i := n - 1; i >= 0; i-- {
-		stats[i] = p.pop().(ast.Statement)
+	for i := 0; i < n; i++ {
+		stats[i] = p.getValue(ctx.Stat(i)).(ast.Statement)
 	}
-	p.push(&ast.Program{
+	p.setValue(ctx, &ast.Program{
 		Statements: stats,
 	})
 }
 
 func (p *Parser) ExitLetStat(ctx *monkey.LetStatContext) {
-	value := p.pop().(ast.Expression)
+	value := p.getValue(ctx.Expr()).(ast.Expression)
 	name := ctx.IDENT().GetText()
 	if fnLit, ok := value.(*ast.FunctionLiteral); ok {
 		fnLit.Name = name
 	}
-	p.push(&ast.LetStatement{
+	p.setValue(ctx, &ast.LetStatement{
 		Token: token.Token{Type: token.LET, Literal: "let"},
 		Name:  newIdentifier(name),
 		Value: value,
@@ -97,25 +95,25 @@ func (p *Parser) ExitLetStat(ctx *monkey.LetStatContext) {
 }
 
 func (p *Parser) ExitRetStat(ctx *monkey.RetStatContext) {
-	returnValue := p.pop().(ast.Expression)
-	p.push(&ast.ReturnStatement{
+	returnValue := p.getValue(ctx.Expr()).(ast.Expression)
+	p.setValue(ctx, &ast.ReturnStatement{
 		Token:       token.Token{Type: token.RETURN, Literal: "return"},
 		ReturnValue: returnValue,
 	})
 }
 
 func (p *Parser) ExitExprStat(ctx *monkey.ExprStatContext) {
-	expr := p.pop().(ast.Expression)
-	p.push(&ast.ExpressionStatement{
+	expr := p.getValue(ctx.Expr()).(ast.Expression)
+	p.setValue(ctx, &ast.ExpressionStatement{
 		Token:      token.Token{}, // TODO
 		Expression: expr,
 	})
 }
 
 func (p *Parser) ExitIndexExpr(ctx *monkey.IndexExprContext) {
-	index := p.pop().(ast.Expression)
-	left := p.pop().(ast.Expression)
-	p.push(&ast.IndexExpression{
+	index := p.getValue(ctx.Expr(1)).(ast.Expression)
+	left := p.getValue(ctx.Expr(0)).(ast.Expression)
+	p.setValue(ctx, &ast.IndexExpression{
 		Token: token.Token{Type: token.LBRACKET, Literal: "["},
 		Left:  left,
 		Index: index,
@@ -123,9 +121,9 @@ func (p *Parser) ExitIndexExpr(ctx *monkey.IndexExprContext) {
 }
 
 func (p *Parser) ExitCallExpr(ctx *monkey.CallExprContext) {
-	args := p.pop().([]ast.Expression)
-	fnExpr := p.pop().(ast.Expression)
-	p.push(&ast.CallExpression{
+	args := p.getValue(ctx.Exprs()).([]ast.Expression)
+	fnExpr := p.getValue(ctx.Expr()).(ast.Expression)
+	p.setValue(ctx, &ast.CallExpression{
 		Token:     token.Token{Type: token.LPAREN, Literal: "("},
 		Function:  fnExpr,
 		Arguments: args,
@@ -133,50 +131,50 @@ func (p *Parser) ExitCallExpr(ctx *monkey.CallExprContext) {
 }
 
 func (p *Parser) ExitUnOpExpr(ctx *monkey.UnOpExprContext) {
-	right := p.pop().(ast.Expression)
+	right := p.getValue(ctx.Expr()).(ast.Expression)
 	op := ctx.GetOp().GetText()
-	p.push(newPrefixExpression(op, right))
+	p.setValue(ctx, newPrefixExpression(op, right))
 }
 
 func (p *Parser) ExitMulDivExpr(ctx *monkey.MulDivExprContext) {
-	right := p.pop().(ast.Expression)
-	left := p.pop().(ast.Expression)
+	right := p.getValue(ctx.Expr(1)).(ast.Expression)
+	left := p.getValue(ctx.Expr(0)).(ast.Expression)
 	op := ctx.GetOp().GetText()
-	p.push(newInfixExpression(op, left, right))
+	p.setValue(ctx, newInfixExpression(op, left, right))
 }
 
 func (p *Parser) ExitAddSubExpr(ctx *monkey.AddSubExprContext) {
-	right := p.pop().(ast.Expression)
-	left := p.pop().(ast.Expression)
+	right := p.getValue(ctx.Expr(1)).(ast.Expression)
+	left := p.getValue(ctx.Expr(0)).(ast.Expression)
 	op := ctx.GetOp().GetText()
-	p.push(newInfixExpression(op, left, right))
+	p.setValue(ctx, newInfixExpression(op, left, right))
 }
 
 func (p *Parser) ExitLtGtExpr(ctx *monkey.LtGtExprContext) {
-	right := p.pop().(ast.Expression)
-	left := p.pop().(ast.Expression)
+	right := p.getValue(ctx.Expr(1)).(ast.Expression)
+	left := p.getValue(ctx.Expr(0)).(ast.Expression)
 	op := ctx.GetOp().GetText()
-	p.push(newInfixExpression(op, left, right))
+	p.setValue(ctx, newInfixExpression(op, left, right))
 }
 
 func (p *Parser) ExitEqNeExpr(ctx *monkey.EqNeExprContext) {
-	right := p.pop().(ast.Expression)
-	left := p.pop().(ast.Expression)
+	right := p.getValue(ctx.Expr(1)).(ast.Expression)
+	left := p.getValue(ctx.Expr(0)).(ast.Expression)
 	op := ctx.GetOp().GetText()
-	p.push(newInfixExpression(op, left, right))
+	p.setValue(ctx, newInfixExpression(op, left, right))
 }
 
 func (p *Parser) ExitIfExpr(ctx *monkey.IfExprContext) {
 	var conseq, alter *ast.BlockStatement
 	switch len(ctx.AllBlock()) {
 	case 1:
-		conseq = p.pop().(*ast.BlockStatement)
+		conseq = p.getValue(ctx.Block(0)).(*ast.BlockStatement)
 	case 2:
-		alter = p.pop().(*ast.BlockStatement)
-		conseq = p.pop().(*ast.BlockStatement)
+		alter = p.getValue(ctx.Block(1)).(*ast.BlockStatement)
+		conseq = p.getValue(ctx.Block(0)).(*ast.BlockStatement)
 	}
-	cond := p.pop().(ast.Expression)
-	p.push(&ast.IfExpression{
+	cond := p.getValue(ctx.Expr()).(ast.Expression)
+	p.setValue(ctx, &ast.IfExpression{
 		Token:       token.Token{Type: token.IF, Literal: "if"},
 		Condition:   cond,
 		Consequence: conseq,
@@ -185,9 +183,9 @@ func (p *Parser) ExitIfExpr(ctx *monkey.IfExprContext) {
 }
 
 func (p *Parser) ExitFnLit(ctx *monkey.FnLitContext) {
-	body := p.pop().(*ast.BlockStatement)
-	params := p.pop().([]*ast.Identifier)
-	p.push(&ast.FunctionLiteral{
+	body := p.getValue(ctx.Block()).(*ast.BlockStatement)
+	params := p.getValue(ctx.Params()).([]*ast.Identifier)
+	p.setValue(ctx, &ast.FunctionLiteral{
 		Token:      token.Token{Type: token.FUNCTION, Literal: "fn"},
 		Parameters: params,
 		Body:       body,
@@ -195,29 +193,29 @@ func (p *Parser) ExitFnLit(ctx *monkey.FnLitContext) {
 }
 
 func (p *Parser) ExitArrLit(ctx *monkey.ArrLitContext) {
-	elements := p.pop().([]ast.Expression)
-	p.push(&ast.ArrayLiteral{
+	elements := p.getValue(ctx.Exprs()).([]ast.Expression)
+	p.setValue(ctx, &ast.ArrayLiteral{
 		Token:    token.Token{Type: token.LBRACKET, Literal: "["},
 		Elements: elements,
 	})
 }
 
 func (p *Parser) ExitHashLit(ctx *monkey.HashLitContext) {
-	pairs := p.pop().(map[ast.Expression]ast.Expression)
-	p.push(&ast.HashLiteral{
+	pairs := p.getValue(ctx.Pairs()).(map[ast.Expression]ast.Expression)
+	p.setValue(ctx, &ast.HashLiteral{
 		Token: token.Token{Type: token.LBRACE, Literal: "{"},
 		Pairs: pairs,
 	})
 }
 
 func (p *Parser) ExitIdent(ctx *monkey.IdentContext) {
-	p.push(newIdentifier(ctx.IDENT().GetText()))
+	p.setValue(ctx, newIdentifier(ctx.IDENT().GetText()))
 }
 
 func (p *Parser) ExitIntLit(ctx *monkey.IntLitContext) {
 	intLit := ctx.INT().GetText()
 	value, _ := strconv.ParseInt(intLit, 10, 64)
-	p.push(&ast.IntegerLiteral{
+	p.setValue(ctx, &ast.IntegerLiteral{
 		Token: token.Token{Type: token.INT, Literal: intLit},
 		Value: value,
 	})
@@ -228,7 +226,7 @@ func (p *Parser) ExitStrLit(ctx *monkey.StrLitContext) {
 	if len(strLit) >= 2 {
 		strLit = strLit[1 : len(strLit)-1]
 	}
-	p.push(&ast.StringLiteral{
+	p.setValue(ctx, &ast.StringLiteral{
 		Token: token.Token{Type: token.STRING, Literal: strLit},
 		Value: strLit,
 	})
@@ -245,28 +243,32 @@ func (p *Parser) ExitBoolLit(ctx *monkey.BoolLitContext) {
 		tokenType = token.FALSE
 		value = false
 	}
-	p.push(&ast.BooleanLiteral{
+	p.setValue(ctx, &ast.BooleanLiteral{
 		Token: token.Token{Type: tokenType, Literal: boolLit},
 		Value: value,
 	})
 }
 
+func (p *Parser) ExitParenExpr(ctx *monkey.ParenExprContext) {
+	p.setValue(ctx, p.getValue(ctx.Expr()))
+}
+
 func (p *Parser) ExitExprs(ctx *monkey.ExprsContext) {
 	n := len(ctx.AllExpr())
 	exprs := make([]ast.Expression, n)
-	for i := n - 1; i >= 0; i-- {
-		exprs[i] = p.pop().(ast.Expression)
+	for i := 0; i < n; i++ {
+		exprs[i] = p.getValue(ctx.Expr(i)).(ast.Expression)
 	}
-	p.push(exprs)
+	p.setValue(ctx, exprs)
 }
 
 func (p *Parser) ExitBlock(ctx *monkey.BlockContext) {
 	n := len(ctx.AllStat())
 	stats := make([]ast.Statement, n)
-	for i := n - 1; i >= 0; i-- {
-		stats[i] = p.pop().(ast.Statement)
+	for i := 0; i < n; i++ {
+		stats[i] = p.getValue(ctx.Stat(i)).(ast.Statement)
 	}
-	p.push(&ast.BlockStatement{
+	p.setValue(ctx, &ast.BlockStatement{
 		Token:      token.Token{Type: token.LBRACE, Literal: "{"},
 		Statements: stats,
 	})
@@ -278,22 +280,25 @@ func (p *Parser) ExitParams(ctx *monkey.ParamsContext) {
 	for i := 0; i < n; i++ {
 		params[i] = newIdentifier(ctx.IDENT(i).GetText())
 	}
-	p.push(params)
+	p.setValue(ctx, params)
 }
 
 func (p *Parser) ExitPairs(ctx *monkey.PairsContext) {
 	n := len(ctx.AllPair())
-	keys := make([]ast.Expression, n)
-	values := make([]ast.Expression, n)
-	for i := n - 1; i >= 0; i-- {
-		values[i] = p.pop().(ast.Expression)
-		keys[i] = p.pop().(ast.Expression)
-	}
 	pairs := map[ast.Expression]ast.Expression{}
 	for i := 0; i < n; i++ {
-		pairs[keys[i]] = values[i]
+		pair := p.getValue(ctx.Pair(i)).([]ast.Expression)
+		pairs[pair[0]] = pair[1]
 	}
-	p.push(pairs)
+	p.setValue(ctx, pairs)
+}
+
+func (p *Parser) ExitPair(ctx *monkey.PairContext) {
+	pair := make([]ast.Expression, 2)
+	for i := range pair {
+		pair[i] = p.getValue(ctx.Expr(i)).(ast.Expression)
+	}
+	p.setValue(ctx, pair)
 }
 
 func newIdentifier(identLit string) *ast.Identifier {
